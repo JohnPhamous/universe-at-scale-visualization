c88e123110f8eae68de822ffdb47ac3ed596bf17
John Pham <johnphammail@gmail.com>
2019-12-22 19:27:44 -0800

    Add initial layout for planets

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+import planets from "./data";
 
 const App: React.FC = () => {
+  const totalHeight = planets.reduce(
+    (acc, currentPlanet) => acc + Number(currentPlanet.radius),
+    0
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="universe">
+      {planets.map(planet => {
+        return (
+          <div
+            id={planet.name}
+            key={planet.name}
+            className="planet"
+            style={{
+              backgroundColor: planet.color,
+              height: `${(Number(planet.radius) / totalHeight) * 100}%`
+            }}
+          ></div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: "Mercury",
+    radius: "1516",
+    color: "#bdbdbd"
+  },
+  {
+    name: "Venus",
+    radius: "3760.4",
+    color: "#f4c96f"
+  },
+  {
+    name: "Earth",
+    radius: "3958.8",
+    color: "#60b5e0"
+  },
+  {
+    name: "Mars",
+    radius: "2106.1",
+    color: "#b82747"
+  },
+  {
+    name: "Jupiter",
+    radius: "43441",
+    color: "#f1ceba"
+  },
+  {
+    name: "Saturn",
+    radius: "36184",
+    color: "#f7c86b"
+  },
+  {
+    name: "Uranus",
+    radius: "15759",
+    color: "#87c7be"
+  },
+  {
+    name: "Neptune",
+    radius: "15299",
+    color: "#5fb5e2"
+  }
+];
